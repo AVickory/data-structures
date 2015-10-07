94aaf73813271f977963fe1412b1cc4c11d1d1fd
avickory <andrew.vickory@gmail.com>
2015-10-07 16:07:28 -0700

    implemented linked lists

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,30 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if(list.tail !== null) {
+      list.tail.next = Node(value);
+      list.tail = list.tail.next;
+    } else {
+      list.tail = Node(value);
+    }
+    if(list.head === null) {
+      list.head = list.tail;
+    }
   };
 
   list.removeHead = function() {
+    if(list.head !== null) {
+      var temp = list.head.value
+      list.head = list.head.next;
+      return temp;
+    }
   };
 
   list.contains = function(target) {
+    for (var curr = list.head; curr !== null; curr = curr.next){
+      if (curr.value === target) return true;  
+    }
+    return false;
   };
 
   return list;
