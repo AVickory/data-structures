f32b5a6a0c4c3f95f4e365d7bcf6e4df94bf0e9c
avickory <andrew.vickory@gmail.com>
2015-10-08 11:20:31 -0700

    Implemented BST

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,45 @@
 var BinarySearchTree = function(value) {
+  var bst = Object.create(BinarySearchTree.prototype);
+  bst.value = value;
+  bst.left = null;
+  bst.right = null;
+  return bst;
 };
 
+BinarySearchTree.prototype.insert = function(value){
+  var newTree = BinarySearchTree(value);
+  if (value < this.value){
+    if (this.left === null){ 
+      this.left = newTree;
+    } else {
+      this.left.insert(value);
+    }
+  } else if (value > this.value){
+    if (this.right === null){ 
+      this.right = newTree;
+    } else {
+      this.right.insert(value);
+    }    
+  }
+};
+
+BinarySearchTree.prototype.contains = function(value){
+  if (value < this.value){
+    return (this.left === null) ? false : this.left.contains(value);
+  }
+  if (this.value === value) return true;
+  if (value > this.value){
+    return (this.right === null) ? false : this.right.contains(value);
+  }
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(callback){
+  var newTree = BinarySearchTree();
+  newTree.value = callback(this.value);
+  newTree.left = (this.left === null) ? null : this.left.depthFirstLog(callback);
+  newTree.right = (this.right === null) ? null : this.right.depthFirstLog(callback);
+  return newTree;
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
