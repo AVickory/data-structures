cc67034dc287d6667e036e3fc132d22e0779b2e3
avickory <andrew.vickory@gmail.com>
2015-10-09 12:33:33 -0700

    WE IMPLEMENTED ALL OF PREFIX TREES!!1!

--- Advanced and Nightmare/prefix-tree.js
+++ Advanced and Nightmare/prefix-tree.js
@@ -3,11 +3,11 @@ var Trie = function () {
   this.isWord = false;
 }
 
-Trie.prototype.traverseTo = function (target, callback) {
-  if(this.children !== undefined) {
+// Trie.prototype.traverseTo = function (target, callback) {
+//   if(this.children !== undefined) {
 
-  }
-}
+//   }
+// }
 
 Trie.prototype.add = function (word) {
   if(word === '') {
@@ -17,7 +17,7 @@ Trie.prototype.add = function (word) {
     if(this.children[newChar] === undefined) {
       this.children[newChar] = new Trie();
     }
-    this.children[newChar].add(word.slice(1));
+     return this.children[newChar].add(word.slice(1));
   }
 };
 
@@ -33,14 +33,54 @@ Trie.prototype.containsWord = function (word) {
   }
 };
 
-Trie.prototype.containsPrefix = function (pref) {
+Trie.prototype.getWordsWithPrefix = function (pref) {
+  var res = [];
 
+  var prefNode = this.traverseTo(pref);
+
+  var getWord = function (node, pref) {
+    if(node.isWord) {
+      res.push(pref);
+    }
+    for(var child in node.children) {
+      getWord(node.children[child], pref + child);
+    }
+  }
+
+  getWord(prefNode, pref)
+
+  return res;
 };
 
-Trie.prototype.getWordsWithPrefix = function (pref) {
-
+Trie.prototype.traverseTo = function (pref) {
+  if(pref === '') {
+    return this;
+  } else {
+    var newChar = pref[0];
+    if(this.children[newChar] !== undefined) {
+     return this.children[newChar].traverseTo(pref.slice(1));
+    }
+   return null;
+  }
 };
 
 
 
 var t = new Trie();
+t.add('de');
+t.add('da');
+t.add('dog');
+t.add('dogs');
+t.add('dalmations');
+
+console.log(t);
+console.log(t.containsWord(''));
+console.log(t.containsWord('d'));
+console.log(t.containsWord('de'));
+console.log(t.containsWord('derp'));
+
+console.log(t.getWordsWithPrefix('d'));
+console.log(t.getWordsWithPrefix('da'));
+console.log(t.getWordsWithPrefix('de'));
+console.log(t.getWordsWithPrefix('do'));
+console.log(t.getWordsWithPrefix('dog'));
